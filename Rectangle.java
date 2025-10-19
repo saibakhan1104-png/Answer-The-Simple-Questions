0d5d6e027aaf11ee7c618df78625f865c607751e
saibakhan1104-png <saibakhan1104@gmail.com>
2025-10-19 21:49:01 +0600

    Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -6,12 +6,11 @@ class Rectangle {
         double width = 4.5;
         double height = 7.9;
         double area = width * height;
-        double perimeter = 2 * (width + height);
 
 	DecimalFormat df = new DecimalFormat("0.000");
 
         System.out.println("Area: " + df.format(area));
-        System.out.println("Perimeter: " + df.format(perimeter));
 
     }
+
 }
